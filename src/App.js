e37f3c33138b650bf8bf3905ad2c63c4328f1db3
adurthi kumarsurya <121813015+asurya185@users.noreply.github.com>
2023-04-23 21:01:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./app.css";
+import Header from "./Components/Header/Header";
+import Cards from "./Components/Cards/Cards";
+import Footer from "./Components/Footer/Footer";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Cards />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
